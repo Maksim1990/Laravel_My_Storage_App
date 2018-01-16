6ee717a994b59bfef8afa2666c7606e6fd8b8d3c
Your Name <you@example.com>
2018-01-16 23:18:16 +0300

    - Added functionality to login via Social networks - Added functionality to login via Google/Github - Improved functionality to login via Facebook

---
+++ app/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','account_type','sns_acc_id','avatar_url','first_name','last_name','status'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
