58e762e7133d25dd4be3832244be49e9a4fc4550
maxim.narushevich <maxi@digitalmind.be>
2018-04-11 13:38:25 +0300

    - Change icons for left sidebar

--- resources/views/layouts/left.blade.php
+++ resources/views/layouts/left.blade.php
@@ -15,7 +15,7 @@
 
 
         <a id="Link1" onclick="myFunc('Link_block_1')" href="javascript:void(0)" class="w3-bar-item w3-button"><i
-                    class="fa fa-inbox w3-margin-right"></i>@lang('messages.my_profile')<i
+                    class="fas fa-user  w3-text-green w3-margin-right"></i>@lang('messages.my_profile')<i
                     class="fa fa-caret-down w3-margin-left"></i></a>
 
         <div id="Link_block_1" class="w3-hide w3-animate-left" style="padding-left: 10px;">
@@ -60,39 +60,39 @@
         </div>
 
         <a id="Link2" onclick="myFunc('Link_block_2')" href="javascript:void(0)" class="w3-bar-item w3-button"><i
-                    class="fa fa-inbox w3-margin-right"></i>@lang_u('messages.books')<i class="fa fa-caret-down w3-margin-left"></i></a>
+                    class="fas fa-book w3-margin-right w3-text-green"></i>@lang_u('messages.books')<i class="fa fa-caret-down w3-margin-left"></i></a>
         <div id="Link_block_2" class="w3-hide w3-animate-left" style="padding-left: 30px;">
             <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/books/list/'.Auth::id())}}" class="w3-bar-item w3-button"><i
-                        class="fa fa-paper-plane w3-margin-right"></i>@lang('messages.my_books')</a>
+                        class="fas fa-caret-square-down w3-margin-right"></i>@lang('messages.my_books')</a>
             </a> <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/books/list')}}" class="w3-bar-item w3-button"><i
-                        class="fa fa-paper-plane w3-margin-right"></i>@lang_u('messages.all') @lang_u('messages.books')</a></a>
-            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/books/create')}}" class="w3-bar-item w3-button"><i class="fa fa-hourglass-end w3-margin-right"></i>@lang_u('messages.add') @lang_u('messages.books')</a>
-            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/import_books_main')}}" class="w3-bar-item w3-button"><i class="fa fa-trash w3-margin-right"></i>@lang('messages.import_export')
+                        class="fas fa-globe w3-margin-right"></i>@lang_u('messages.all') @lang_u('messages.books')</a></a>
+            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/books/create')}}" class="w3-bar-item w3-button"><i class="fas fa-plus-circle w3-margin-right"></i>@lang_u('messages.add') @lang_u('messages.books')</a>
+            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/import_books_main')}}" class="w3-bar-item w3-button"><i class="fas fa-upload w3-margin-right"></i>@lang('messages.import_export')
                 @lang('messages.books')</a>
         </div>
 
 
         <a id="Link3" onclick="myFunc('Link_block_3')" href="javascript:void(0)" class="w3-bar-item w3-button"><i
-                    class="fa fa-inbox w3-margin-right"></i>@lang_u('messages.movies')<i class="fa fa-caret-down w3-margin-left"></i></a>
+                    class="fas fa-video w3-margin-right  w3-text-green"></i>@lang_u('messages.movies')<i class="fa fa-caret-down w3-margin-left"></i></a>
         <div id="Link_block_3" class="w3-hide w3-animate-left" style="padding-left: 30px;">
             <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/movies/list/'.Auth::id())}}" class="w3-bar-item w3-button"><i
-                        class="fa fa-paper-plane w3-margin-right"></i>@lang('messages.my_movies')</a></a>
+                        class="fas fa-caret-square-down w3-margin-right"></i>@lang('messages.my_movies')</a></a>
             <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/movies/list')}}" class="w3-bar-item w3-button"><i
-                        class="fa fa-paper-plane w3-margin-right"></i>@lang_u('messages.all') @lang_u('messages.movies')</a></a>
-            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/movies/create')}}" class="w3-bar-item w3-button"><i class="fa fa-hourglass-end w3-margin-right">
+                        class="fas fa-globe w3-margin-right"></i>@lang_u('messages.all') @lang_u('messages.movies')</a></a>
+            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/movies/create')}}" class="w3-bar-item w3-button"><i class="fas fa-plus-circle w3-margin-right">
                 </i>@lang_u('messages.add') @lang_u('messages.movies')</a>
-            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/import_movies_main')}}" class="w3-bar-item w3-button"><i class="fa fa-trash w3-margin-right"></i>@lang('messages.import_export')
+            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/import_movies_main')}}" class="w3-bar-item w3-button"><i class="fas fa-upload w3-margin-right"></i>@lang('messages.import_export')
             @lang('messages.movies')</a>
         </div>
 
         <a id="Link4" onclick="myFunc('Link_block_4')" href="javascript:void(0)" class="w3-bar-item w3-button"><i
-                    class="fa fa-inbox w3-margin-right"></i>@lang('messages.other_functions')<i
+                    class="fab fa-ethereum w3-margin-right  w3-text-green"></i>@lang('messages.other_functions')<i
                     class="fa fa-caret-down w3-margin-left"></i></a>
         <div id="Link_block_4" class="w3-hide w3-animate-left" style="padding-left: 30px;">
             <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/import_export')}}" class="w3-bar-item w3-button"><i
-                        class="fa fa-paper-plane w3-margin-right"></i>@lang('messages.import_export')</a></a>
-            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/search')}}" class="w3-bar-item w3-button"><i class="fa fa-hourglass-end w3-margin-right"></i>@lang('messages.instant_search')</a>
-            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/categories')}}" class="w3-bar-item w3-button"><i class="fa fa-trash w3-margin-right"></i>@lang('messages.view_all_categories')</a>
+                        class="fas fa-upload w3-margin-right"></i>@lang('messages.import_export')</a></a>
+            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/search')}}" class="w3-bar-item w3-button"><i class="fas fa-search w3-margin-right"></i>@lang('messages.instant_search')</a>
+            <a href="{{URL::to(LaravelLocalization::getCurrentLocale() .'/categories')}}" class="w3-bar-item w3-button"><i class="fas fa-location-arrow w3-margin-right"></i>@lang('messages.view_all_categories')</a>
         </div>
 
         <div id="lang_block" style="position:absolute;bottom:5%;left:10%;">
